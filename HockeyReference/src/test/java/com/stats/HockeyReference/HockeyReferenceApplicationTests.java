71fb71e1bb1fd7470784fe0755af1b10968e43ab
sldri <sldri@10.0.0.46>
2023-04-30 07:41:01 -0400

    Initial commit as part of redo on project:
    Incorporating Spring framework to project
    
    Build using Maven
    
    Redo is part of building a better base for my project.  My last commit I
    noticed some significant design issues that I cornered myself into
    making.  I look to address those in this new design.  Additionally
    working on incorporating unit testing to the project as well.

---
+++ HockeyReference/src/test/java/com/stats/HockeyReference/HockeyReferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.HockeyReference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyReferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
