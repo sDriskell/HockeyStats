d06977ab01c663e2cdfe8b46a75dc6f642ee47e3
sDriskell <49995352+sDriskell@users.noreply.github.com>
2023-08-04 06:43:29 -0400

    Major changes to project and process... (#9)
    I stepped away for a couple months and coming back I saw I didn't do a
    good job planning out the design of this project.  So, I'm using this to
    reset and plan out the flow in which this project will work.
    
    This is a rough draft commit.  I've got the input process for the file
    consumption done just with teams to test that it works.  Next will be
    handling the factory component so that we can use that consumer class to
    handle the other csv files for players and goalies.
    
    I'm also using another site that generates a ton of stats and data to
    use.  I've also shied away from scraping and decided to go with a Bring
    Your Own Data (BYOD) approach.
    
    End goal will be to create the project from the ground up to have a
    sound OOP design.  I want to avoid the cases where I broke DRY, had
    brittle/rigid code, and create something that is far more flexible.
    
    Co-authored-by: Shane <author@my-network-settings.com>

--- NHLScraper/src/main/NhlApp.java
+++ NHLScraper/src/main/NhlApp.java
@@ -1,34 +1,16 @@
 package main;
 
-import java.io.IOException;
-import java.util.List;
+import processors.HockeyTeamIntake;
 
-import org.apache.log4j.Logger;
-
-import factory.SkaterFactory;
-import process.HockeyReferenceScraper;
-import process.StatProcessor;
-import stats.IPlayerStats;
-
-import org.apache.log4j.LogManager;
 
 /**
- * Simple NHL tool to visualize statistical data from Hockey-Reference.com.
+ * Simple NHL tool to visualize statistical data from: 
+ *  https://www.naturalstattrick.com/
  * 
- * @author sldri
+ * @author Shane
  */
 public class NhlApp {
-    private static final Logger logger = LogManager.getLogger(NhlApp.class);
-
-    public static void main(String... args) throws IOException {       
-        process();
-    }
-    
-    public static void process() throws IOException {
-        logger.info("Scraping source.");
-        HockeyReferenceScraper.scrapeFiles();
-        
-        logger.info("Starting stats processor.");
-        StatProcessor statProcessor = new StatProcessor();
+    public static void main(String... args) {       
+        HockeyTeamIntake.processTeamStatsCsv();
     }
 }
